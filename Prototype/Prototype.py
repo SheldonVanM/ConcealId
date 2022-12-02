eca425594742f09b9bb266f84105f5fecdc70418
Sheldon Van Midelkoop <sheldonvanmiddelkoop@gmail.com>
2022-12-02 02:43:32 -0500

    Began adding QR code generation.

--- Prototype/Prototype.py
+++ Prototype/Prototype.py
@@ -1,35 +1,38 @@
-import cv2, cryptography
+import cv2, base64, qrcode
+from cryptography.fernet import Fernet
 from pyzbar.pyzbar import decode
 from getpass import getpass
 
-mode = input("Enter Mode (Generate : G or Decode : D): ")
+def generate_qr(password):
+    gift_id = input("Enter gift: ")
+    Fernet.generate_key()
+    f = Fernet(password)
+    f.encrypt(b"" + gift_id)
+    print("Encrypted Gift: " + gift_id)
 
-psswrd = getpass()
+if __name__ == "__main__":
+    mode = input("Enter Mode (Generate : G or Decode : D): ")
+    password_input = getpass()
+    password = base64.urlsafe_b64decode(password_input)
 
-if( mode == "G" ):
-    #Do stuff
-    print("Not available yet")
+    if( mode == "G" ):
+        #Do stuff
+        generate_qr(password=password)
 
-if( mode == "D"):
-    # Do other stuff
-    vid = cv2.VideoCapture(0)
-    while( True ):
-        rect, frame = vid.read()
-        h, w = frame.shape[:2]
-
-        cv2.imshow('Gift Identifier', frame)
-
-        message = decode((frame[:, :, 0].astype('uint8').tobytes(), w, h))
-
-        if( len(message) > 0 and message[0] is not None and message[0] is not ""):
-            print("Decoded message is " + str(message[0]))
-            break
-
-        if( cv2.waitKey(1) & 0xFF == ord('q') ):
-            break
-    vid.release()
-    cv2.destroyAllWindows()
-
-
-else:
-    print("Incorrect input!")
+    if( mode == "D"):
+        # Do other stuff
+        vid = cv2.VideoCapture(0)
+        while( True ):
+            rect, frame = vid.read()
+            h, w = frame.shape[:2]
+            cv2.imshow('Gift Identifier', frame)
+            message = decode((frame[:, :, 0].astype('uint8').tobytes(), w, h))
+            if( len(message) > 0 and message[0] is not None and message[0] is not ""):
+                print("Decoded message is " + str(message[0]))
+                break
+            if( cv2.waitKey(1) & 0xFF == ord('q') ):
+                break
+        vid.release()
+        cv2.destroyAllWindows()
+    else:
+        print("Incorrect input!")
