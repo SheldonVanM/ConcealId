0e601c8a10ff3b8614f3af14ae4ebe3fc04946c5
Sheldon Van Midelkoop <sheldonvanmiddelkoop@gmail.com>
2023-01-02 18:38:42 -0500

    Updated color theme. Began layout for header.

--- Prototype/Prototype.py
+++ Prototype/Prototype.py
@@ -1,24 +1,56 @@
-import cv2, base64, qrcode
+import cv2, base64, qrcode, json, os
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from pyzbar.pyzbar import decode
 from getpass import getpass
 
-def generate_qr(password):
-    gift_id = input("Enter gift: ")
-    Fernet.generate_key()
-    f = Fernet(password)
-    f.encrypt(b"" + gift_id)
-    print("Encrypted Gift: " + gift_id)
+def generate_qr(receiver, present, order):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.ERROR_CORRECT_M,
+        box_size=12,
+        border=4
+    )
+    data = {
+        'Person' : receiver,
+        'Gift' : str(present),
+        'Priority' : order
+    }
+    qr.add_data(json.dumps(data, indent=2))
+    qr.make(fit=True)
+    img = qr.make_image()
+    img.show()
+    
+def generate_key(password):
+    salt = os.urandom(16)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=390000
+    )
+    return base64.urlsafe_b64encode(kdf.derive(password))
+    
+def encrypt_data(key, data):
+    f = Fernet(key=key)
+    return f.encrypt(b"" + data)
+
 
 if __name__ == "__main__":
     mode = input("Enter Mode (Generate : G or Decode : D): ")
-    password_input = getpass()
-    password = base64.urlsafe_b64decode(password_input)
+    password = getpass()
+    password = bytes(password, 'utf-8')
 
     if( mode == "G" ):
         #Do stuff
-        generate_qr(password=password)
-
+        receiver = input("Receiver: ")
+        gift = input("Gift: ")
+        gift = bytes(gift, 'utf-8')
+        priority = input("Priority: ")
+        key = generate_key(password=password)
+        encrypted_data = encrypt_data(key=key, data=gift)
+        generate_qr(receiver=receiver, present=encrypted_data, order=priority)
     if( mode == "D"):
         # Do other stuff
         vid = cv2.VideoCapture(0)
@@ -27,7 +59,7 @@ if __name__ == "__main__":
             h, w = frame.shape[:2]
             cv2.imshow('Gift Identifier', frame)
             message = decode((frame[:, :, 0].astype('uint8').tobytes(), w, h))
-            if( len(message) > 0 and message[0] is not None and message[0] is not ""):
+            if( len(message) > 0 and message[0] is not None and message[0] != ""):
                 print("Decoded message is " + str(message[0]))
                 break
             if( cv2.waitKey(1) & 0xFF == ord('q') ):
--- concealid/src/app/app.module.ts
+++ concealid/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './header/menu/menu.component';
+import { LoginComponent } from './header/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
